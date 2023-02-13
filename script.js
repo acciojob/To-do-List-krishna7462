270ed3231a4f70634c124f73a76b04e43ad30ece
krishna7462 <117273752+krishna7462@users.noreply.github.com>
2023-02-13 18:39:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 //your code here
 function addlist(){
-	let newli = document.createElement("li");
-	newli.innerText = document.querySelector("#newTodoInput").value;
-	document.querySelector("#todoList").appendChild(newli);
+	let task = document.querySelector("#newTodoInput").value;
+	if(task.trim().length!=0){
+		var newli = document.createElement("li");
+		newli.innerText=task;
+	}
+		document.querySelector("ol").appendChild(newli);
 	document.querySelector("#newTodoInput").value ="";
+	
+	
 }
 
