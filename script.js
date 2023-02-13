be7ff14fbe63d67055489b92c1595da46950a1b9
krishna7462 <117273752+krishna7462@users.noreply.github.com>
2023-02-13 18:26:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
+function addlist(){
+	let newli = document.createElement("li");
+	newli.innerText = document.querySelector("#newTodoInput").value;
+	document.querySelector("#todoList").appendChild(newli);
+	document.querySelector("#newTodoInput").value ="";
+}
 
